d0984c41f9707e000700256e142852ec423952d2
zhangzhengyang <1074385735@qq.com>
2026-08-21 16:41:55 +0800

    fix: 升级 Android 构建工具链适配 Flutter 3.47 最低版本要求 (Gradle 8.14.3 / AGP 8.13.0 / Kotlin 2.2.21 / JVM17)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,14 +11,14 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         // flutter_local_notifications 等插件需要 core library desugaring
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
